76a6843e352beb92fc05ef510d95c1a814d666d4
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-05-22 12:00:48 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import './App.css';
+
+import Navbar from "./Navbar.js";
+import Header from "./Header.js"
+import Home from "./Home.js"
+import PostPage from "./PostPage.js"
+import Footer from "./Footer.js"
+import About from "./About.js"
+import Missing from "./Missing.js"
+import NewPost from './NewPost';
+
+import {Route,Routes, useNavigate} from "react-router-dom";
+import { useState, useEffect} from 'react';
+import { format } from 'date-fns';
+
+
+function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const filteredResults = posts.filter((post) =>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate("/")
+  }
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter(post => post.id !== id);
+    setPosts(postsList);
+    // navigate('/');
+  }
+  return (
+    <div className="App">
+      <Navbar search={search} setSearch={setSearch} />
+      <Header title={"React JS Blog"} />
+
+      <Routes>
+        <Route path="/" element={<Home posts={searchResults} />} />
+
+        <Route exact path="/post" element={<NewPost 
+        handleSubmit={handleSubmit} 
+        postTitle={postTitle} 
+        postBody={postBody} 
+        setPostTitle={setPostTitle}
+        setPostBody={setPostBody} 
+        /> } 
+        />
+        
+        <Route path="/post/:id" element={<PostPage 
+        posts={posts} 
+        handleDelete={handleDelete} /> }
+         />
+
+        <Route  path="/about" element={<About /> } />
+        <Route  path="*" element={<Missing />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
